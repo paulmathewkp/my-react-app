cdf306f98765025cccb12ddca9affebb0bcf19cf
Paul Mathew <paulmathewkp@gmail.com>
2024-11-03 13:08:48 +0530

    initial commit for search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [meals, setMeals] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const debounceFetch = setTimeout(() => {
+    const fetchMeals = async () => {
+      setLoading(true);
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=`);
+        const data = await response.json();
+        if (data.meals) {
+          const filteredMeals = data.meals.filter(meal => 
+            meal.strInstructions.toLowerCase().includes(searchTerm.trim().toLowerCase()) ||
+            meal.strMeal.toLowerCase().includes(searchTerm.trim().toLowerCase()) //trimmed the space even if user enters space
+          )
+          setMeals(filteredMeals);
+        } else {
+          setMeals([]);
+        }
+
+      setLoading(false);
+    };
+
+    fetchMeals();
+  }, 300); //debounce time of 300ms
+
+  return () => clearTimeout(debounceFetch);
+  }, [searchTerm]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Meal Search</h1>
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        placeholder="Search"
+      />
+      <div className='meal-container'>{loading && <p>Loading * * * *</p>}</div>
+      <div className="meal-container">
+        {meals.length > 0 ? (
+          meals.map(meal => (
+            <div key={meal.idMeal} className="meal-card">
+              <h2>{meal.strMeal}</h2><img src={meal.strMealThumb} alt={meal.strMeal} />
+              <p title={meal.strInstructions}>{meal.strInstructions}</p>
+            </div>
+          ))
+        ) : (
+          !loading && <p>No Data</p>
+        )}
+      </div>
     </div>
   );
 }
